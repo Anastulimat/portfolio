bb491bb1b400c867c494b8b775787e7492b8e585
Anas TULIMAT <anas.tulimat@orange.com>
2024-06-02 15:31:29 +0200

    feat: integrate Sentry to catch error on the website

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://077868f7ad673433d5c40de7b6f75a3a@o4507362508931072.ingest.de.sentry.io/4507362515812432",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
